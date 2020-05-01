5e6b55b7ad8fd7a70e49ed71f1c79f303519ff27
sol <mssarratea@gmail.com>
2020-05-01 15:50:41 -0400

    Add features to simple game of life

--- 05-game-of-life/js/entry.js
+++ 05-game-of-life/js/entry.js
@@ -1,54 +1,19 @@
 
-if ( navigator.mediaDevices && navigator.mediaDevices.getUserMedia ) {
-
-    var constraints = { video: { width: 1280, height: 720, facingMode: 'user' } };
-  
-    navigator.mediaDevices.getUserMedia( constraints ).then( function ( stream ) {
-  
-      // apply the stream to the video element used in the texture
-  
-      video.srcObject = stream;
-      video.play();
-  
-    } ).catch( function ( error ) {
-  
-      console.error( 'Unable to access the camera/webcam.', error );
-  
-    } );
-  
-  } else {
-    console.error( 'MediaDevices interface not available.' );
-  }
-  
   var scene, camera, renderer, width,height,controls,dragControls;
-  var video, videoTexture, movieScreen;   
-  function setupMainScene()
-  {
+  function setupMainScene() {
     scene = new THREE.Scene();
     var width = window.innerWidth;
     var height = window.innerHeight;
     camera = new THREE.OrthographicCamera( width / - 2, width / 2, height / 2, height / - 2, 1, 1000 );
     camera.position.z = 2;
+    camera.position.set(0,150,300);
+  
     renderer = new THREE.WebGLRenderer();
     renderer.setSize( window.innerWidth, window.innerHeight );
     document.body.appendChild( renderer.domElement );
-       ///////////
-      // VIDEO //
-      //////////
-    video = document.getElementById( 'video' );
-    videoTexture = new THREE.VideoTexture( video );
-    videoTexture.minFilter = THREE.LinearFilter;
-    videoTexture.magFilter = THREE.LinearFilter;
+  }  
     
-    movieMaterial = new THREE.MeshBasicMaterial( { map: videoTexture, overdraw: true, side:THREE.DoubleSide } );
-    movieGeometry = new THREE.PlaneGeometry( 240, 100, 4, 4 );
-    movieScreen = new THREE.Mesh( movieGeometry, movieMaterial );
-    movieScreen.position.set(0,50,0);
-    camera.position.set(0,150,300);
-    camera.lookAt(movieScreen.position);
-  }
-    
-  var bufferScene, textureA,textureB, videoText;
+  var bufferScene, textureA,textureB, startText;
   function setupBufferScene() {
   
     bufferScene = new THREE.Scene();
@@ -64,32 +29,39 @@ if ( navigator.mediaDevices && navigator.mediaDevices.getUserMedia ) {
     magFilter: THREE.LinearMipMapLinearFilter, 
     format: THREE.RGBAFormat,
     type: THREE.FloatType} );
+
+    textureA.texture.wrapS = THREE.ClampToEdgeWrapping;
+    textureA.texture.wrapT = THREE.ClampToEdgeWrapping;
+    textureB.texture.wrapS = THREE.ClampToEdgeWrapping;
+    textureB.texture.wrapT = THREE.ClampToEdgeWrapping;
   
-    videoText = videoTexture;
+    createTexture = new TextureFactory();
+    createTexture.fromVideo();
+    startText = createTexture.texture;
+   
+
   }
   
-    
   var plane, bufferObject;
   function initBufferScene(){ 
   
-    //videoText = new THREE.TextureLoader().load('/core/zebra2.jpeg' );
       bufferMaterial = new THREE.ShaderMaterial( {
       uniforms: {
+        start: { type: "t", value: startText },
+        texture : { type : 't', value : textureA.texture}, 
         timer : { type : 'i', value: 0}, 
         resolution : {  type : 'v2', value : new THREE.Vector2()}, 
         mouse : { type : 'v2', value : new THREE.Vector2()},
-        color1 : { type : 'v3', value : color1}, 
-        color2 : { type : 'v3', value : color2},
-        texture : { type : 't', value : textureA.texture}, 
-        dA : { type: 'f', value :   dA}, 
-        dB : { type: 'f', value :   dB}, 
-        kill : { type: 'f', value :   kill}, 
-        feed : { type: 'f', value :   feed}, 
         dT : { type: 'f', value :   dT},
-        start: { type: "t", value: videoText },
-        t:  {type:'f', value: interpolate},
+        t:  { type:'f', value: roundPos},
         pixelSize:  {type:'f', value: pixelSize},
-
+        bound:  {type:'f', value: frame},
+        rNeighbour:  {type:'f', value: rNeighbour},
+        tNeighbour:  {type:'f', value: tNeighbour},
+        threshold:  {type:'f', value: threshold},
+        remember : { type: 'b', value : remember},
+        travel : { type: 'bool', value: travel }
+        
       },
           
       vertexShader : document.getElementById('vertexShader').innerHTML,
@@ -100,7 +72,6 @@ if ( navigator.mediaDevices && navigator.mediaDevices.getUserMedia ) {
       bufferObject = new THREE.Mesh( plane, bufferMaterial );
       bufferScene.add(bufferObject);
   
-      startText.needsUpdate = true;
   }
     
     
@@ -108,17 +79,20 @@ if ( navigator.mediaDevices && navigator.mediaDevices.getUserMedia ) {
   window.color2 = new THREE.Color(0, 0, 0);
   window.timer = 0;
   window.mouse = {x : 0, y : 0};
-    
-  window.dA = .2;
-  window.dB = 0.2;
-  window.feed = 0.031;
-  window.kill = 0.057;
-  
   window.clear = 0;
   window.iterations = 1;
-  window.interpolate = 0;
+  window.roundPos = 0.5;
   window.dT = 0.01;
   window.pixelSize = 10.;
+  window.frame = 2.;
+  window.threshold= 0.2;
+  window.bound =3.;
+  window.rNeighbour = 1.;
+  window.tNeighbour = 0.;
+  window.activate = 0;
+  window.remember = 0;
+  window.travel = 0;
+
     
     
   var finalMaterial, geom, quad;
@@ -127,8 +101,11 @@ if ( navigator.mediaDevices && navigator.mediaDevices.getUserMedia ) {
       uniforms : {
         resolution : { type : 'v2', value : new THREE.Vector2( window.innerWidth, window.innerHeight) },
         texture : { type : 't', value : textureB.texture, minFilter : THREE.NearestFilter },
+        start: { type: "t", value: startText },
         color1 : { type : 'c', value : color1 },
-        color2 : { type : 'c', value : color2 }
+        color2 : { type : 'c', value : color2 },
+        activate : {type : 'f', value : activate},
+     
       },
       vertexShader : document.getElementById('vertexShader').innerHTML, 
       fragmentShader : document.getElementById( 'color' ).textContent
@@ -140,26 +117,34 @@ if ( navigator.mediaDevices && navigator.mediaDevices.getUserMedia ) {
   }
   
   window.guiData= {
-    "color1": {
-      "r": 255,
-      "g": 255,
-      "b": 255
+    "remembered": {
+      "default": {
+        "0": {
+          "color1": {
+            "r": 0,
+            "g": 0,
+            "b": 0
+          },
+          "color2": {
+            "r": 255,
+            "g": 255,
+            "b": 255
+          },
+          "iterations": 1,
+          "roundPos": 0.5,
+          "pixelSize": 16,
+          "threshold": 0.631,
+          "frame": 3,
+          "bound": 3,
+          "rNeighbour": 1.,
+          "tNeighbour": 0.04
+        }
+      }
     },
-    "color2": {
-      "r": 0,
-      "g": 47.00000000000006,
-      "b": 255
-    },
-    "dA": 0.778,
-    "dB": 0.421,
-    "dT": 0.003,
-    "feed": 0.0078000000000000005,
-    "kill": 0.051000000000000004,
-    "iterations": 1, 
-    "interpolate": 0.3,
-    "pixelSize": 10.,
-   
-  };
+    "preset": "default",
+    "closed": false,
+    "folders": {}
+  }
   
   function addGuiControls(){
     var gui = new dat.GUI({load: guiData });
@@ -167,14 +152,15 @@ if ( navigator.mediaDevices && navigator.mediaDevices.getUserMedia ) {
         
         gui.addColor(this, "color1");
         gui.addColor(this, "color2");
-   /*      gui.add(this, "dA", 0.0, 1.0).step(0.001);
-        gui.add(this, "dB", 0.0, 1.0).step(0.001);
-        gui.add(this, "feed", 0.0, 0.15).step(0.0001);
-        gui.add(this, "kill", 0.0, 0.15).step(0.0001);
-        gui.add(this, "dT", 0.0, 0.1).step(0.0001); */
         gui.add(this, "iterations", 0, 100).step(1);
-        gui.add(this, "interpolate",0.,6.).step(0.0001);
+        gui.add(this, "roundPos",-0.2,1.).step(0.0001);
         gui.add(this, "pixelSize",0.1,100).step(1.);
+        gui.add(this, "threshold", 0.01, 2.).step(0.001);
+        gui.add(this, "frame", 1.,10.).step(1);
+        gui.add(this, "bound", 1.,9.).step(1);;
+        gui.add(this, "rNeighbour",0.,2.).step(0.01);
+        gui.add(this, "tNeighbour",-rNeighbour,rNeighbour).step(0.01);
+        gui.add(this, "activate",0,1.).step(1.);
         
   }
   
@@ -206,7 +192,6 @@ if ( navigator.mediaDevices && navigator.mediaDevices.getUserMedia ) {
         bufferMaterial.uniforms.resolution.value.x = w;
         bufferMaterial.uniforms.resolution.value.y = h;
         bufferMaterial.uniforms.timer.value = 0;
-        bufferMaterial.uniforms.texture.value = videoText;
     }
     onWindowResize();
     window.addEventListener( 'resize', onWindowResize, false );
@@ -223,50 +208,53 @@ if ( navigator.mediaDevices && navigator.mediaDevices.getUserMedia ) {
           var temp = textureA;
           textureA = textureB;
           textureB = temp;
-      
+        
+          quad.material.map = textureB.texture;
           bufferMaterial.uniforms.texture.value = textureA.texture;
       }
   }
     
-var frame = 0;
+var frameCount = 0;
   function render() {
   
     requestAnimationFrame( render );
 
-    if (frame % 5 == 1){
+    if (frameCount % frame == 1){
       nStepSimulation(); 
 
     };
-    ++ frame;
+    ++ frameCount;
   
-      ++ bufferMaterial.uniforms.timer.value;
-      bufferMaterial.uniforms.dA.value = dA;
-      bufferMaterial.uniforms.dB.value = dB;
-      bufferMaterial.uniforms.feed.value = feed;
-      bufferMaterial.uniforms.kill.value = kill;
-      bufferMaterial.uniforms.t.value = interpolate;
-      bufferMaterial.uniforms.pixelSize.value = pixelSize;
-      
-      finalMaterial.uniforms.color1.value.r = color1.r/255;
-      finalMaterial.uniforms.color1.value.g = color1.g/255;
-      finalMaterial.uniforms.color1.value.b = color1.b/255;
-      
-      finalMaterial.uniforms.color2.value.r = color2.r/255;
-      finalMaterial.uniforms.color2.value.g = color2.g/255;
-      finalMaterial.uniforms.color2.value.b = color2.b/255;
-      renderer.render( bufferScene, camera );
+    ++ bufferMaterial.uniforms.timer.value;
+    bufferMaterial.uniforms.t.value = roundPos;
+    bufferMaterial.uniforms.pixelSize.value = pixelSize;
+    bufferMaterial.uniforms.threshold.value = threshold;
+    bufferMaterial.uniforms.bound.value = bound;
+    bufferMaterial.uniforms.rNeighbour.value = rNeighbour;
+    bufferMaterial.uniforms.tNeighbour.value = tNeighbour;
+    bufferMaterial.uniforms.travel.value = travel;
+    bufferMaterial.uniforms.remember.value = remember;
+
+  
+    
+    finalMaterial.uniforms.color1.value.r = color1.r/255;
+    finalMaterial.uniforms.color1.value.g = color1.g/255;
+    finalMaterial.uniforms.color1.value.b = color1.b/255;
+    
+    finalMaterial.uniforms.color2.value.r = color2.r/255;
+    finalMaterial.uniforms.color2.value.g = color2.g/255;
+    finalMaterial.uniforms.color2.value.b = color2.b/255;
+    finalMaterial.uniforms.activate.value = activate;
+
+    renderer.render( scene, camera );
   
   }
+
   
-  
-  
-  window.startText;
-  initStartTex();
-  
-  setupMainScene();
-  setupBufferScene();
-  initBufferScene();
-  initFinalScene();
-  setupEvents();
-  addGuiControls();
-  render();
+setupMainScene();
+setupBufferScene();
+initBufferScene();
+initFinalScene();
+setupEvents();
+addGuiControls();
+render();
--- core/js/utils.js
+++ core/js/utils.js
@@ -1,22 +1,51 @@
+class TextureFactory {
+    constructor() {
+        this.texture = null;
+      }
 
-function setStartTex(){
-    ctx.fillStyle = 'black';
-    ctx.fillRect( 0, 0, 1024, 512 );
-    ctx.font = '800 400px Courier';
-    ctx.fillStyle = 'yellow';
-    ctx.textAlign = 'center';
-    ctx.textBaseline = 'middle';
-    ctx.fillText( 'SOL', 1024 / 5, 512 / 4 );
-    ctx.fillText( 'QUEMAL', 1024 / 2, 512 / 2 );
-    ctx.fillText( 'SOL', 1024 / 2, 3 * 512 / 4 );
-  }
+    fromCanvas(text = 'welcome'){
+        var cnvs = document.createElement( 'canvas' );
+        cnvs.width = 1024;
+        cnvs.height = 512;
+        var ctx = cnvs.getContext( '2d' );
+        ctx.fillStyle = 'black';
+        ctx.fillRect( 0, 0, 1024, 512 );
+        ctx.font = '300 200px Courier';
+        ctx.fillStyle = 'yellow';
+        ctx.textAlign = 'center';
+        ctx.textBaseline = 'middle';
+        ctx.fillText( text , 1024 / 2, 512 / 2 );
+        
+        this.texture = new THREE.Texture( cnvs );
   
-  function initStartTex(){
-    var cnvs = document.createElement( 'canvas' );
-    cnvs.width = 1024;
-    cnvs.height = 512;
-    ctx = cnvs.getContext( '2d' );
-    setStartTex();
-    startText = new THREE.Texture( cnvs );
-  }
-  
+        this.texture.needsUpdate= true;
+    }
+
+    fromVideo() {
+    if ( navigator.mediaDevices && navigator.mediaDevices.getUserMedia ) {
+        var constraints = { video: { width: 1280, height: 720, facingMode: 'user' } };
+        
+        navigator.mediaDevices.getUserMedia( constraints ).then( function ( stream ) {
+        video.srcObject = stream;
+        video.play();
+        
+        } ).catch( function ( error ) {
+            console.error( 'Unable to access the camera/webcam.', error );
+        
+        } );
+        
+    } else {
+        console.error( 'MediaDevices interface not available.' );
+    }
+    window.video = document.getElementById('video');
+    this.texture = new THREE.VideoTexture(video);
+    this.texture.minFilter = THREE.LinearFilter;
+    this.texture.magFilter = THREE.LinearFilter;
+    }
+
+    fromImage(path){
+        this.texture = new THREE.TextureLoader().load(path);
+    }
+
+}
+
