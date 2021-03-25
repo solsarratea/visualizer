934c1bc56a4a9200e58bd27912f92ed9895ad1d6
Unknown <ssarratea@manas.tech>
2021-03-25 19:39:37 -0300

    Add fileSaver utilities

--- core/js/index-controls.js
+++ core/js/index-controls.js
@@ -1,7 +1,10 @@
 var modal = document.getElementById("showInfo");
 var btn = document.getElementById("btn");
 var span = document.getElementsByClassName("close")[0];
+var screenshot =  document.getElementById("screenshot");
+
 window.isPlay = false;
+modal.style.display = "none";
 
 btn.onclick = function() {
   modal.style.display = "block"
@@ -16,3 +19,19 @@ window.onclick = function(event) {
     modal.style.display = "none";
   }
 }
+
+
+screenshot.onclick = function(event){
+    var aCanvas = document.getElementById("glCanvas"),
+        ctx =  aCanvas.getContext("webgl2", {preserveDrawingBuffer: true});
+    ;
+    aCanvas.toBlob( function(blob)
+                    {
+                        var d = new Date();
+                        var fName = d.getFullYear()+"_"+d.getMonth()+"_"+d.getDate()+"_"+
+                            d.getHours()+"_"+d.getMinutes()+"_"+d.getSeconds();
+
+                        saveAs(blob, "bu3npattern"+fName+".png");
+                    });
+
+};
